fb3f7c75cbb8c4d9ee59677973116d07a21e8db2
Sebastian <sebastian.stemmer@dig-it-up.de>
2023-05-22 07:40:09 +0200

    TJCAO-259 Added test stage in pipeline and on basic test in frontend

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -4,7 +4,7 @@ const Button = () => {
   return (
     <div className="flex justify-center items-center h-screen">
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Click M
+        Click Me
       </button>
     </div>
   );
