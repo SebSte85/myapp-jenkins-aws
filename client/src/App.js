b2968f3513ffc796da814ed74296b345c235b3e4
Sebastian <sebastian.stemmer@dig-it-up.de>
2023-05-24 09:12:29 +0200

    TJCAO-291 Basic setup done

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import Button from "./components/Button";
 import "./index.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="justify-center bg-gray-300">
-      <Button />
+    <div className="h-screen bg-gray-200 dark:bg-slate-800">
+      <Home />
     </div>
   );
 }
--- client/src/components/Button.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import Button from "./Button";
-
-test("renders button component", () => {
-  render(<Button />);
-  const buttonElement = screen.getByText(/click me/i);
-  expect(buttonElement).toBeInTheDocument();
-});
